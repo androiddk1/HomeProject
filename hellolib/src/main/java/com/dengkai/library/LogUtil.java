2e2dd5751922ffc2903b9cdb0e27595e5840c2ea
androiddk1 <122602215@qq.com>
2017-05-30 17:21:58 +0800

    create LogUtil

---
+++ hellolib/src/main/java/com/dengkai/library/LogUtil.java
@@ -0,0 +1,15 @@
+package com.dengkai.library;
+
+import android.util.Log;
+
+/**
+ * Created by helloworld on 2017/5/30.
+ */
+
+public class LogUtil {
+    public static void printLog(){
+        Log.e("test","hello,git!!");
+        Log.e("test","hello,git!!");
+        Log.e("test","hello,git!!");
+    }
+}
